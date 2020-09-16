6c3782cd2eef052811c422506feff0d36909fe3a
tcostasa <tcostasa@everis.com>
2020-09-16 10:31:02 -0300

    estrutura

---
+++ src/main/java/br/salvador/thiago/parametrizacao/ParametrizacaoApplication.java
@@ -0,0 +1,13 @@
+package br.salvador.thiago.parametrizacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParametrizacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParametrizacaoApplication.class, args);
+	}
+
+}
