213c6717992ea669574b7eba1c12195704c24275
Bruno Borges Fonseca <b.f_04@hotmail.com>
2020-09-16 19:33:39 -0300

    Test de integracao para o POST da API de adicionar Parametro de Produto Esteira

---
+++ src/test/java/br/salvador/thiago/parametrizacao/ParametroProdutoEsteiraControllerTest.java
@@ -0,0 +1,72 @@
+package br.salvador.thiago.parametrizacao;
+
+import br.salvador.thiago.parametrizacao.dto.ParametroProdutoEsteiraPostDto;
+import br.salvador.thiago.parametrizacao.dto.ParametroProdutoEsteiraResponseDto;
+import br.salvador.thiago.parametrizacao.model.ParametroProdutoEsteira;
+import br.salvador.thiago.parametrizacao.repository.ParametroProdutoEsteiraRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ParametroProdutoEsteiraControllerTest {
+
+    @MockBean
+    private ParametroProdutoEsteiraRepository parametroProdutoEsteiraRepository;
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void whenPostNewParametroPrudutoEsteira_thenReturnCreatedWithNewObject() {
+        TestRestTemplate restTemplate = new TestRestTemplate();
+
+        Mockito.when(this.parametroProdutoEsteiraRepository.save(any(ParametroProdutoEsteira.class)))
+                .thenReturn(new ParametroProdutoEsteira());
+
+        ParametroProdutoEsteiraPostDto toSave = new ParametroProdutoEsteiraPostDto(
+                33,
+                3,
+                "S",
+                "S",
+                "N",
+                "N",
+                20,
+                30,
+                LocalDateTime.parse("2020-09-10T16:22:01.8394834"),
+                LocalDateTime.parse("2020-09-10T16:22:01.8394834"),
+                2,
+                3
+        );
+        HttpEntity<ParametroProdutoEsteiraPostDto> request =
+                new HttpEntity<ParametroProdutoEsteiraPostDto>(toSave);
+
+        ResponseEntity<ParametroProdutoEsteiraResponseDto> response = restTemplate
+                .exchange("http://localhost:" + this.port + "/v1/parametro-regra-esteira",
+                        HttpMethod.POST,
+                        request,
+                        ParametroProdutoEsteiraResponseDto.class,
+                        1L);
+
+        Assertions.assertThat(response.getStatusCodeValue())
+                .isEqualTo(201L);
+    }
+
+}
